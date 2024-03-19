28c5efbf84b5c31c9efac46cec846fb4b93dc08f
owpac <29702840+owpac@users.noreply.github.com>
2024-03-19 18:19:12 +0100

    fix(install): fix install script

--- install.sh
+++ install.sh
@@ -4,7 +4,41 @@
 # -u: exit on unset variables
 set -eu
 
-source lib/logs.sh
+log_color() {
+  color_code="$1"
+  shift
+
+  printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
+}
+
+log_red() {
+  log_color "0;31" "$@"
+}
+
+log_green() {
+  log_color "0;32" "$@"
+}
+
+log_blue() {
+  log_color "0;34" "$@"
+}
+
+log_task() {
+  log_blue "👉" "$@"
+}
+
+log_done() {
+  log_green "✅" "$@"
+}
+
+log_error() {
+  log_red "❌" "$@"
+}
+
+error() {
+  log_error "$@"
+  exit 1
+}
 
 # install Homebrew
 if [ ! "$(command -v brew)" ]; then
